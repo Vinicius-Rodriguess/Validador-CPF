50c944ed78090199b9fe78fc5649020ddfde063d
Tk 000407 <rodrigues.vini.2004@gmail.com>
2023-10-06 16:03:21 -0300

    validador de cpf

---
+++ index.js
@@ -0,0 +1,66 @@
+const btnValida = document.querySelector('.btnValida');
+
+btnValida.addEventListener('click', () => {
+    
+    const cpfAValidar = document.querySelector('.input-cpf').value;
+    
+    const cpf = new ValidaCPF(cpfAValidar);
+    
+    const resultado = document.querySelector('.resultado');
+    
+    if(cpf.valida()){
+        resultado.classList.remove('escondido');
+        resultado.innerHTML = 'Valido!'
+    } else {
+        resultado.classList.remove('escondido');
+        resultado.innerHTML = 'Invalido!'
+    }
+
+});
+
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function(){
+            return cpfEnviado.replace(/\D+/g, '');
+        }
+    });
+}
+
+ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.sequencia()) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+}
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+
+    let regressivo = cpfArray.length + 1;
+
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCPF.prototype.sequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+}
+
+
+
+
+
